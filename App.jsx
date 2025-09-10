71c6141db31242b5dde275c841541c81cf17a3a8
Priyansh Kamlesh <priyansh1kamlesh0@gmail.com>
2025-09-10 21:59:16 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import {BrowserRouter , Routes , Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About_us from './pages/About_us'
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about_us' element={<About_us/>}/>
+    </Routes>
+    </BrowserRouter>
+    </div>
   )
 }
 
